e119637a3defbae9b8fbf82401f4cf6cce578119
Johann <johann@objekt.stream>
2021-10-09 03:34:14 +0200

    + support json

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -7,6 +7,7 @@ type Data = {
   error?: string
   meta?: Record<string, string>
   jsonLD?: Record<string, string>
+  json?: Record<string, string>
   headers?: Record<string, string[]>
   status?: {
     code: number
@@ -25,7 +26,12 @@ export default async function handler(
     return
   }
 
-  const optionsRes = await fetch(url, { method: 'OPTIONS' })
+  const optionsRes = await fetch(url, { method: 'OPTIONS' }).catch(() => undefined)
+  if (!optionsRes) {
+    res.status(400).json({ error: 'could not load url' })
+    return
+  }
+
   const status = {
     code: optionsRes.status,
     message: optionsRes.statusText,
@@ -36,16 +42,29 @@ export default async function handler(
   const html = await targetRes.text().catch(() => '')
   
   if (!html) {
-    res.status(400).json({ error: 'could not load url' })
+    res.status(400).json({ error: 'could not url contents' })
     return
+  }
+
+  let json
+  try {
+    json = JSON.parse(html)
+  } catch (e) {}
+
+  if (json) {
+    return res.json({
+      json,
+      status,
+      headers,
+    })
   }
 
   const $ = cheerio.load(html)
 
-  const meta: Data['meta'] = {}
-
+  let meta: Data['meta']
   $('meta').each((i, el) => { 
     const { name, content, property } = el.attribs
+    if (!meta) meta = {}
     if (name && content) {
       meta[name] = content
     }
